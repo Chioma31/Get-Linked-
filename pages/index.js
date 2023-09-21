cd90be0137505b5978607060570bae43e6491b48
bagwells <timmyayomidebagwells@outlook.com>
2023-09-21 10:57:42 +0100

    ...

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,9 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div  className=" w-auto h-auto lg:bg-[url('/bg.png')] bg-[url('/bg.png')] leading-[23px]  bg-cover"> 
+    <>
+    
+      <div  className=" w-auto h-auto lg:bg-[url('/bg.png')] bg-[url('/bg.png')] leading-[23px]  bg-cover"> 
 
         <NavBar />
 
@@ -126,6 +128,230 @@ export default function Home() {
 
         </div>
 
-    </div>
+      </div>
+
+      <div className='border-b   border-[#4A2672]' />
+
+      <div  className=" w-auto h-auto lg:bg-[url('/bg.png')] bg-[url('/bg.png')] leading-[23px]  bg-cover">
+
+        <div className=' flex gap-40 justify-evenly pt-14 items-center  px-6 pb-5 2xl:pr-60 2xl:pl-[300px]'>
+
+          <Image
+            className=" "
+            height={400}
+            width={480}
+            src="/intro.png"
+            alt=" intro image"
+          />
+
+          <div className=' text-white w-[520px] '>
+
+            <p className=' font-[700]  text-[35px] leading-[43px] '>
+              Introduction to getlinked <div className='text-[#D434FE]'>tech Hackathon 1.0</div>
+            </p>
+            <p className=' font-[400] mt-5  text-[16px] leading-[28px] '>
+              Our tech hackathon is a melting pot of visionaries, and its purpose is as
+              clear as day: to shape the future. Whether you're a coding genius, a 
+              design maverick, or a concept wizard, you'll have the chance to transform 
+              your ideas into reality. Solving real-world problems, pushing the boundaries
+              of technology, and creating solutions that can change the world,
+              that's what we're all about!
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className='border-b   border-[#4A2672]' />
+
+      <div  className=" w-auto h-auto lg:bg-[url('/bg.png')] bg-[url('/bg.png')] leading-[23px]  bg-cover">
+
+        <div className=' flex gap-40 justify-evenly  items-center  px-6 2xl:px-56'>
+
+          <div className=' text-white w-[520px] '>
+
+            <p className=' font-[700]  text-[35px] leading-[43px] '>
+              Rules and  <div className='text-[#D434FE]'>Guidelines</div>
+            </p>
+            <p className=' font-[400] mt-5  text-[16px] leading-[28px] '>
+              Our tech hackathon is a melting pot of visionaries, and its purpose is as
+              clear as day: to shape the future. Whether you're a coding genius, a 
+              design maverick, or a concept wizard, you'll have the chance to transform 
+              your ideas into reality. Solving real-world problems, pushing the boundaries
+              of technology, and creating solutions that can change the world,
+              that's what we're all about!
+            </p>
+
+          </div>
+
+          <Image
+            className=" "
+            height={400}
+            width={600}
+            src="/rules.png"
+            alt=" rules image"
+          />
+
+        </div>
+        
+      </div>
+
+      <div className='border-b   border-[#4A2672]' />
+
+      <div  className=" w-auto h-auto lg:bg-[url('/bg.png')] bg-[url('/bg.png')] leading-[23px]  bg-cover">
+
+        <div className=' flex gap-40 pt-14 pb-20 justify-evenly  items-center  px-6 2xl:px-56'>
+
+          <Image
+            className=" "
+            height={400}
+            width={600}
+            src="/judging.png"
+            alt=" rules image"
+          />
+
+          <div className=' text-white w-[520px] '>
+
+            <p className=' font-[700]  text-[35px] leading-[43px] '>
+            Judging Criteria <div className='text-[#D434FE]'>Key attributes </div>
+            </p>
+
+            <p className=' font-[500] mt-5  text-[14px] leading-[28px] '>
+              <span className='text-[#FF26B9] font-[700] pr-1  text-[16px] leading-[28px]'>
+                Innovation and Creativity :
+              </span>
+                Evaluate the uniqueness and creativity of the
+                solution. Consider whether it addresses a real-world problem in a novel 
+                way or introduces innovative features.
+            </p>
+
+            <p className=' font-[500] mt-5  text-[14px] leading-[28px] '>
+              <span className='text-[#FF26B9] font-[700] pr-1  text-[16px] leading-[28px]'>
+                Functionality: 
+              </span>
+                Assess how well the solution works. Does it perform its 
+                intended functions effectively and without major issues? Judges would
+                consider the completeness and robustness of the solution.
+            </p>
+
+            <p className=' font-[500] mt-5  text-[14px] leading-[28px] '>
+              <span className='text-[#FF26B9] font-[700] pr-1  text-[16px] leading-[28px]'>
+                Impact and Relevance:
+              </span>
+                Determine the potential impact of the solution 
+                in the real world. Does it address a significant problem, and is it relevant 
+                to the target audience? Judges would assess the potential social, 
+                economic, or environmental benefits.
+            </p>
+
+            <p className=' font-[500] mt-5  text-[14px] leading-[28px] '>
+              <span className='text-[#FF26B9] font-[700] pr-1  text-[16px] leading-[28px]'>
+                Technical Complexity: 
+              </span>
+                Evaluate the technical sophistication of the solution. 
+                Judges would consider the complexity of the code, the use of advanced 
+                technologies or algorithms, and the scalability of the solution.
+            </p>
+
+            <p className=' font-[500] mt-5  text-[14px] leading-[28px] '>
+              <span className='text-[#FF26B9] font-[700] pr-1  text-[16px] leading-[28px]'>
+                Adherence to Hackathon Rules:  
+              </span>
+                Judges will Ensure that the team adhered 
+                to the rules and guidelines of the hackathon, including deadlines, use of 
+                specific technologies or APIs, and any other competition-specific requirements.
+            </p>
+
+            <div className='pt-14'>
+
+              <Link href="/readmore" className="bg-gradient-to-r from-[#FE34B9] to-[#903AFF] rounded-[4px] font-[400] text-[16px]  text-white px-14  py-[12px]">
+                Read more
+              </Link>
+
+            </div>
+
+          </div>
+
+        </div>
+        
+      </div>
+
+      <div className='border-b   border-[#4A2672]' />
+
+      <div  className=" w-auto h-auto lg:bg-[url('/bg.png')] bg-[url('/bg.png')] leading-[23px]  bg-cover">
+
+        <div className=' flex gap-40 pt-14 pb-20 justify-evenly  items-center  px-6 2xl:px-56'>
+
+          <div className=' text-white w-[520px] '>
+
+            <p className=' font-[700]  text-[35px] leading-[43px] '>
+            Frequently Ask <div className='text-[#D434FE]'>Question </div>
+            </p>
+
+            <p className=' font-[400] mt-5  text-[16px] leading-[28px] '>
+              We got answers to the questions that you might
+              want to ask about getlinked Hackathon 1.0
+            </p>
+
+            <div className=' w-[393px]'>
+
+              <p className='border-b pb-2 border-[#D434FE] font-[500] mt-5  text-[14px] leading-[28px] '>
+                Can I work on a project I started before the hackathon?
+                <span className='text-[#FF26B9] font-[500] pl-6  text-[20px] '>
+                  +
+                </span>
+              </p>
+
+              <p className='border-b pb-2 border-[#D434FE] font-[500] mt-5  text-[14px] leading-[28px] '>
+                What happens if I need help during the hackathon?
+                <span className='text-[#FF26B9] font-[500] pl-[51px]  text-[20px] '>
+                  +
+                </span>
+              </p>
+
+              <p className='border-b pb-2 border-[#D434FE] font-[500] mt-5  text-[14px] leading-[28px] '>
+                What happens if I don't have an idea for a project?
+                <span className='text-[#FF26B9] font-[500] pl-[54px]    text-[20px] '>
+                  +
+                </span>
+              </p>
+
+              <p className='border-b pb-2 border-[#D434FE] font-[500] mt-5  text-[14px] leading-[28px] '>
+                Can I join a team or do I have to come with one?
+                <span className='text-[#FF26B9] font-[500] pl-[69px]    text-[20px] '>
+                  +
+                </span>
+              </p>
+
+              <p className='border-b pb-2 border-[#D434FE] font-[500] mt-5  text-[14px] leading-[28px] '>
+                What happens after the hackathon ends
+                <span className='text-[#FF26B9] font-[500] pl-[118px]      text-[20px] '>
+                  +
+                </span>
+              </p>
+
+              <p className='border-b pb-2 border-[#D434FE] font-[500] mt-5  text-[14px] leading-[28px] '>
+                Can I work on a project I started before the hackathon?
+                <span className='text-[#FF26B9] font-[500] pl-6  text-[20px] '>
+                  +
+                </span>
+              </p>
+
+            </div>
+
+          </div>
+
+          <Image
+            className=" "
+            height={400}
+            width={600}
+            src="/faq.png"
+            alt=" rules image"
+          />
+
+        </div>
+        
+      </div>
+
+    </>
   )
 }
